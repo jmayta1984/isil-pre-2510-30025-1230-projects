2a9235cc61353594ea00286c482c28b3121543bf
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-05 20:29:46 -0400

    chore: initial project setup

---
+++ EasyShoes/EasyShoes/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  EasyShoes
+//
+//  Created by user272495 on 5/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            Tab("Home", systemImage: "shoe") {
+                Text("Home")
+            }
+            Tab("Favorites", systemImage: "heart") {
+                Text("Favorites")
+            }
+            Tab("Cart", systemImage: "cart") {
+                Text("Cart")
+            }
+            Tab("Profile", systemImage: "person") {
+                Text("Profile")
+            }
+        }
+        .tint(.black)
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
